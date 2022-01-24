1eaf39fad63304d272198cd66de2a0e1b590ccf5
Jefferson <jeffersonbraster@gmail.com>
2022-01-23 23:21:29 -0300

    criando os estilos do posts na home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Link from "next/link";
 import Header from "../components/Header";
 import { sanityClient, urlFor } from "../sanity";
 import { Post } from "../typings";
@@ -8,8 +9,6 @@ type Props = {
 };
 
 export default function Home({ posts }: Props) {
-  console.log(posts);
-
   return (
     <div className="max-w-7xl mx-auto">
       <Head>
@@ -38,6 +37,34 @@ export default function Home({ posts }: Props) {
           src="http://accountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png"
           alt="Letra M"
         />
+      </div>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 md:p-6">
+        {posts.map((post) => (
+          <Link key={post._id} href={`/post/${post.slug.current}`}>
+            <div className="border rounded-lg group cursor-pointer overflow-hidden">
+              <img
+                className="h-60 w-full object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out"
+                src={urlFor(post.mainImage).url()!}
+                alt={post.title}
+              />
+
+              <div className="flex justify-between p-5 bg-white">
+                <div>
+                  <p className="text-lg font-bold">{post.title}</p>
+                  <p className="text-xs">
+                    {post.description} by {post.author.name}
+                  </p>
+                </div>
+                <img
+                  className="h-12 w-12 rounded-full"
+                  src={urlFor(post.author.image).url()!}
+                  alt={post.author.name}
+                />
+              </div>
+            </div>
+          </Link>
+        ))}
       </div>
     </div>
   );
--- typings.d.ts
+++ typings.d.ts
@@ -1,5 +1,5 @@
 export interface Post {
-  id: string;
+  _id: string;
   _createdAt: string;
   title: string;
   author: {
