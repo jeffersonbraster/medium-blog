2aca1790e17cf3dfc92e809fbeddb20a4bc2fab5
Jefferson <jeffersonbraster@gmail.com>
2022-01-24 22:18:00 -0300

    criando form e estilos para comentarios

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -63,6 +63,39 @@ const Post = ({ post }: Props) => {
           />
         </div>
       </article>
+
+      <hr className="max-w-lg my-5 mx-auto border border-yellow-500" />
+
+      <form className="flex flex-col p-5 max-w-2xl mx-auto mb-10">
+        <h3 className="text-sm text-yellow-500">Curtiu este artigo?</h3>
+        <h4 className="text-3xl font-bold">Deixe um comentário:</h4>
+        <hr className="py-3 mt-2" />
+
+        <label className="block mb-5">
+          <span className="text-gray-700">Nome</span>
+          <input
+            className="shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-yellow-500 outline-none focus:ring"
+            placeholder="Jefferson Brandão"
+            type="text"
+          />
+        </label>
+        <label className="block mb-5">
+          <span className="text-gray-700">E-mail</span>
+          <input
+            className="shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-yellow-500 outline-none focus:ring"
+            placeholder="jefferson@gmail.com"
+            type="text"
+          />
+        </label>
+        <label className="block mb-5">
+          <span className="text-gray-700">Comment</span>
+          <textarea
+            className="shadow border rounded py-2 px-3 form-textarea mt-1 block w-full ring-yellow-500 outline-none focus:ring"
+            placeholder="Muito bom post..."
+            rows={8}
+          />
+        </label>
+      </form>
     </main>
   );
 };
