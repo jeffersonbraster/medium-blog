7b3ad00d87bb1bdf346dc899c69c6d5cdd3e77fb
Jefferson <jeffersonbraster@gmail.com>
2022-01-24 21:58:26 -0300

    estilizando a pagina de post

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -2,17 +2,67 @@ import { GetStaticProps } from "next";
 import Header from "../../components/Header";
 import { sanityClient, urlFor } from "../../sanity";
 import { Post } from "../../typings";
+import PortableText from "react-portable-text";
 
 type Props = {
   post: Post;
 };
 
 const Post = ({ post }: Props) => {
-  console.log(post);
-
   return (
     <main>
       <Header />
+
+      <img
+        className="w-full h-40 object-cover"
+        src={urlFor(post.mainImage).url()!}
+        alt={post.title}
+      />
+
+      <article className="max-w-3xl mx-auto p-5">
+        <h1 className="text-3xl mt-10 mb-3">{post.title}</h1>
+        <h2 className="text-xl font-light text-gray-500 mb-2">
+          {post.description}
+        </h2>
+
+        <div className="flex items-center space-x-2">
+          <img
+            className="h-10 w-10 rounded-full"
+            src={urlFor(post.author.image).url()!}
+            alt={post.author.name}
+          />
+          <p className="font-extralight text-sm">
+            Blog post by{" "}
+            <span className="text-green-600">{post.author.name}</span> -
+            published at {new Date(post._createdAt).toLocaleString()}
+          </p>
+        </div>
+
+        <div className="mt-10">
+          <PortableText
+            className=""
+            dataset={process.env.NEXT_PUBLIC_SANITY_DATASET}
+            projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}
+            content={post.body}
+            serializers={{
+              h1: (props: any) => {
+                <h1 className="text-2xl font-bold my-5" {...props} />;
+              },
+              h2: (props: any) => {
+                <h1 className="text-xl font-bold my-5" {...props} />;
+              },
+              link: ({ children, href }: any) => {
+                <a href={href} className="text-blue-500 hover:underline">
+                  {children}
+                </a>;
+              },
+              li: ({ children }: any) => {
+                <li className="ml-4 list-disc">{children}</li>;
+              },
+            }}
+          />
+        </div>
+      </article>
     </main>
   );
 };
@@ -67,6 +117,7 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   }
 
   return {
+    revalidate: 60 * 60,
     props: {
       post,
     },
