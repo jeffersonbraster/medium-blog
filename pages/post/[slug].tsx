c83a961e7cc26b42c916e83ed5b17c1610dd3983
Jefferson <jeffersonbraster@gmail.com>
2022-01-24 23:13:08 -0300

    finalizando os comentarios apenas aprovados

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -4,6 +4,7 @@ import { sanityClient, urlFor } from "../../sanity";
 import { Post } from "../../typings";
 import PortableText from "react-portable-text";
 import { useForm, SubmitHandler } from "react-hook-form";
+import { useState } from "react";
 
 type FormProps = {
   _id: string;
@@ -17,6 +18,8 @@ type Props = {
 };
 
 const Post = ({ post }: Props) => {
+  const [submitted, setSubmitted] = useState(false);
+
   const {
     register,
     handleSubmit,
@@ -30,9 +33,11 @@ const Post = ({ post }: Props) => {
     })
       .then(() => {
         console.log(data);
+        setSubmitted(true);
       })
       .catch((err) => {
         console.log(err);
+        setSubmitted(false);
       });
   };
 
@@ -93,61 +98,91 @@ const Post = ({ post }: Props) => {
 
       <hr className="max-w-lg my-5 mx-auto border border-yellow-500" />
 
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="flex flex-col p-5 max-w-2xl mx-auto mb-10"
-      >
-        <h3 className="text-sm text-yellow-500">Curtiu este artigo?</h3>
-        <h4 className="text-3xl font-bold">Deixe um comentário:</h4>
-        <hr className="py-3 mt-2" />
-
-        <input {...register("_id")} type="hidden" name="_id" value={post._id} />
-
-        <label className="block mb-5">
-          <span className="text-gray-700">Nome</span>
-          <input
-            {...register("name", { required: true })}
-            className="shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-yellow-500 outline-none focus:ring"
-            placeholder="Jefferson Brandão"
-            type="text"
-          />
-        </label>
-        <label className="block mb-5">
-          <span className="text-gray-700">E-mail</span>
-          <input
-            {...register("email", { required: true })}
-            className="shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-yellow-500 outline-none focus:ring"
-            placeholder="jefferson@gmail.com"
-            type="text"
-          />
-        </label>
-        <label className="block mb-5">
-          <span className="text-gray-700">Comment</span>
-          <textarea
-            {...register("comment", { required: true })}
-            className="shadow border rounded py-2 px-3 form-textarea mt-1 block w-full ring-yellow-500 outline-none focus:ring"
-            placeholder="Muito bom post..."
-            rows={8}
-          />
-        </label>
-
-        <div className="flex flex-col p-5">
-          {errors.name && (
-            <span className="text-red-400">Nome é obrigatório.</span>
-          )}
-          {errors.email && (
-            <span className="text-red-400">E-mail é obrigatório.</span>
-          )}
-          {errors.comment && (
-            <span className="text-red-400">Comentário é obrigatório.</span>
-          )}
+      {submitted ? (
+        <div className="flex flex-col p-10 my-10 bg-yellow-400 text-white max-w-2xl mx-auto">
+          <h3 className="text-3xl font-bold">
+            Obrigado por deixar seu comentário.
+          </h3>
+          <p>
+            Seu comentário está sendo avaliado e logo será liberado na página.
+          </p>
         </div>
+      ) : (
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col p-5 max-w-2xl mx-auto mb-10"
+        >
+          <h3 className="text-sm text-yellow-500">Curtiu este artigo?</h3>
+          <h4 className="text-3xl font-bold">Deixe um comentário:</h4>
+          <hr className="py-3 mt-2" />
 
-        <input
-          type="submit"
-          className="shadow bg-yellow-500 hover:bg-yellow-300 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded cursor-pointer"
-        />
-      </form>
+          <input
+            {...register("_id")}
+            type="hidden"
+            name="_id"
+            value={post._id}
+          />
+
+          <label className="block mb-5">
+            <span className="text-gray-700">Nome</span>
+            <input
+              {...register("name", { required: true })}
+              className="shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-yellow-500 outline-none focus:ring"
+              placeholder="Jefferson Brandão"
+              type="text"
+            />
+          </label>
+          <label className="block mb-5">
+            <span className="text-gray-700">E-mail</span>
+            <input
+              {...register("email", { required: true })}
+              className="shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-yellow-500 outline-none focus:ring"
+              placeholder="jefferson@gmail.com"
+              type="text"
+            />
+          </label>
+          <label className="block mb-5">
+            <span className="text-gray-700">Comment</span>
+            <textarea
+              {...register("comment", { required: true })}
+              className="shadow border rounded py-2 px-3 form-textarea mt-1 block w-full ring-yellow-500 outline-none focus:ring"
+              placeholder="Muito bom post..."
+              rows={8}
+            />
+          </label>
+
+          <div className="flex flex-col p-5">
+            {errors.name && (
+              <span className="text-red-400">Nome é obrigatório.</span>
+            )}
+            {errors.email && (
+              <span className="text-red-400">E-mail é obrigatório.</span>
+            )}
+            {errors.comment && (
+              <span className="text-red-400">Comentário é obrigatório.</span>
+            )}
+          </div>
+
+          <input
+            type="submit"
+            className="shadow bg-yellow-500 hover:bg-yellow-300 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded cursor-pointer"
+          />
+        </form>
+      )}
+
+      <div className="flex flex-col p-10 my-10 max-w-2xl mx-auto shadow-yellow-400 shadow space-y-2">
+        <h3 className="text-4xl">Comentários:</h3>
+        <hr className="pb-2" />
+
+        {post.comments?.map((comment) => (
+          <div key={comment._id}>
+            <p>
+              <span className="text-yellow-500">{comment.name}:</span>{" "}
+              {comment.comment}
+            </p>
+          </div>
+        ))}
+      </div>
     </main>
   );
 };
@@ -185,6 +220,10 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
     name,
     image
   },
+  'comments': *[
+    _type == "comment" &&
+    post._ref == ^._id &&
+    approved == true],
   description,
   mainImage,
   slug,
